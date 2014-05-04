0fe65ce792e798879f159d31a17f6c344c825c41
Spooky <smlsun@gmail.com>
2014-05-04 16:32:53 +0800

    update readme and taskStore.js comment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # extjs-bryntum-gantt-demo
 ### a Sails application
+
+### start app step
+
+1. please install node.js first
+2. command: npm install
+3. command: sails lift
+
+then you can type url: http://localhost:1337/ in browser,
--- assets/app/store/TaskStore.js
+++ assets/app/store/TaskStore.js
@@ -12,6 +12,9 @@ Ext.define("MyApp.store.TaskStore", {
         this.callParent(arguments);
     },
 
+
+    //讀取資料來源
+    //this.url = data.js 參考 assets/data.js，在這可以替換為從後端 server 取得初始資料
     loadDataSet : function () {
         Ext.Ajax.request({
             url      : this.url,
@@ -24,15 +27,18 @@ Ext.define("MyApp.store.TaskStore", {
         });
     },
 
+    // 取得甘特圖操作後的資料並且將資料送交指定網址
     saveDataSet : function () {
+
+        //getModifiedRecords() -> 屬於 Ext.data.TreeStore 的函式，新增或修改的資料可透過此函式取得
+        //getRemovedRecords() -> 屬於 Ext.data.AbstractStore 的函式，若要取得刪除的 dependencies 或 task 可用此函式
 
         var resources = this.resourceStore.getModifiedRecords();
         var assignments = this.assignmentStore.getModifiedRecords();
         var dependencies = this.dependencyStore.getModifiedRecords();
         var tasks = this.getModifiedRecords();
 
-        console.log(this.dependencyStore);
-
+        //將資料一一存為陣列
         function extract(records) {
             var result = [];
 
@@ -50,6 +56,8 @@ Ext.define("MyApp.store.TaskStore", {
             tasks        : extract(tasks)
         };
 
+
+        // 由後端進行資料儲存
         Ext.Ajax.request({
             url      : "/gantt/save",
             method   : 'POST',
@@ -62,6 +70,7 @@ Ext.define("MyApp.store.TaskStore", {
         })
     },
 
+    //從後端取得資料後，一一將資料 load 到對應的 store
     populateDataStores : function (response, options) {
         var data = Ext.decode(response.responseText);
 
